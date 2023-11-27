a52ca266a51542a5a8da12a42eb0cc633af5d160
Murat Akyol <95498019+Murat633@users.noreply.github.com>
2023-11-27 11:36:57 +0300

    finished

--- js/header-slider.js
+++ js/header-slider.js
@@ -1,8 +1,8 @@
-const leftBtn = document.getElementById("header-slider-left")
-const rightBtn = document.getElementById("header-slider-right")
-const currentTitleElement = document.getElementById("current-header-title")
-const currentDescriptionElement = document.getElementById("current-header-description")
-const slideList = document.getElementById("header-slider")
+const hLeftbtn = document.getElementById("header-slider-left")
+const hRightBtn = document.getElementById("header-slider-right")
+const hSlideList = document.getElementById("header-slide-list")
+const headerbg = document.getElementById("header-bg")
+
 
 const slides = [
     {
@@ -14,7 +14,7 @@ const slides = [
         index:2,
         title:"Test Proje 2",
         description:"Bu proje Abidjan, Fildişi Sahili'nde yer almaktadır. 261 oda ve Genel Alanlar dahil olmak üzere 31.000 m2'lik binanın beton ve MEP işleri hariç tüm ticari işleri 2",
-        imageUrl:"https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?cs=srgb&dl=pexels-pixabay-269077.jpg&fm=jpg"
+        imageUrl:"https://img.freepik.com/premium-photo/business-people-meeting_53876-21725.jpg"
     },{
         index:3,
         title:"Test Proje 3",
@@ -28,33 +28,38 @@ const slides = [
     }
 ]
 
-var currentElementIndex = 0
+var hCurrentElementIndex = 0
 
-const slideCreator = (slide,index)=>{
+const hSlideCreator = (slide,index)=>{
     const slideTag =  `
-        <div data-index="${index}" class="header-slider__slides__slide">
-            <img class="header-slider__slides__slide__image" src="${slide.imageUrl}" alt="">
-            <h6 class="header-slider__slides__slide__title">${slide.title}</h6>
+    <div id="header-slide" data-h-s-index="${index}" class="header__content-slider__slide__content">
+        <h1 class="header__content-slider__slide__content__title">${slide.title}</h1>
+        <p  class="header__content-slider__slide__content__description">
+            ${slide.description}
+        </p>
+        <div>
+            <a href="#" class="header__content-slider__slide__content__button">Projelerimiz ></a>
         </div>
+    </div>
     `;
 
     return slideTag;
 }
 
-const slideWriter = (slideElements)=>{
-    slideList.innerHTML = "";
+const hSlideWriter = (slideElements)=>{
+    hSlideList.innerHTML = "";
     slideElements.forEach((slide,i) => {
-        slideList.innerHTML += slideCreator(slide,i)
+        hSlideList.innerHTML += hSlideCreator(slide,i)
     });
 }
 
-const activeElementSelector = ()=>{
-    for(let i = 0;i<slideList.children.length;i++){
-        const element = slideList.children[i]
-        if(element.getAttribute("data-index") <= currentElementIndex){
+const hActiveElementSelector = ()=>{
+    for(let i = 0;i<hSlideList.children.length;i++){
+        const element = hSlideList.children[i]
+        const index = element.getAttribute("data-h-s-index")
+        if(index == hCurrentElementIndex){
             element.classList.add("active")
-            currentTitleElement.innerHTML = slides[i].title
-            currentDescriptionElement.innerHTML = slides[i].description
+            headerbg.style = `background-image:url(${slides[index].imageUrl});`
         }else {
             element.classList.remove("active")
         }     
@@ -63,22 +68,22 @@ const activeElementSelector = ()=>{
 
 
 
-slideWriter(slides)
-activeElementSelector()
+hSlideWriter(slides)
+hActiveElementSelector()
 
-const nextSlide = ()=> {
-    if(currentElementIndex < slides.length-1){
-        currentElementIndex += 1
-        activeElementSelector()
+const hNextSlide = ()=> {
+    if(hCurrentElementIndex < slides.length-1){
+        hCurrentElementIndex += 1
+        hActiveElementSelector()
     }
 }
 
-const prevSlide = ()=> {
-    if(currentElementIndex > 0){
-        currentElementIndex -= 1
-        activeElementSelector()
+const hPrevSlide = ()=> {
+    if(hCurrentElementIndex > 0){
+        hCurrentElementIndex -= 1
+        hActiveElementSelector()
     }
 }
 
-leftBtn.addEventListener("click",prevSlide)
-rightBtn.addEventListener("click",nextSlide)
+hLeftbtn.addEventListener("click",hPrevSlide)
+hRightBtn.addEventListener("click",hNextSlide)
